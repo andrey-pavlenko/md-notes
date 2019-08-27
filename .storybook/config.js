2829922d7c00f14b9f723667f003acc8396e2db5
Anfrey Pavlenko <andrey.pavlenko@mail.ru>
2019-08-27 18:18:07 +0300

    Fix storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
 import { configure } from '@storybook/vue';
-import Vue from 'vue';
-import TocItem from '../src/components/toc-item';
+// import Vue from 'vue';
+// import TocItem from '../src/components/toc-item';
 // import Spinner from '../src/components/spinner';
 
-Vue.component('toc-item', TocItem);
+// Vue.component('toc-item', TocItem);
 // Vue.component('spinner', Spinner);
 
 function loadStories() {
